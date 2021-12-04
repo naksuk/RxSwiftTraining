03e85b7d3f464dda3e720483a84c7af8f57b8908
“naksuk <n4ksuk@gmail.com>
2021-12-04 20:48:15 +0900

    RxSwiftを用いたカウンターを実装

--- RxSwiftTraining/ViewController.swift
+++ RxSwiftTraining/ViewController.swift
@@ -6,14 +6,41 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
+    
+    private let count: BehaviorRelay<Int> = BehaviorRelay(value: 0)
+    private let disposedBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupBidings()
     }
-
-
+    
+    private func setupBidings() {
+        
+        button.rx.tap
+            .subscribe (onNext: { [ weak self] _ in
+                self?.increment()
+            })
+            .disposed(by: disposedBag)
+        
+        count.asObservable()
+             .subscribe(onNext: { [weak self] count in
+                 self?.label.text = String(count) })
+             .disposed(by: disposedBag)
+        
+    }
+    
+    private func increment() {
+        count.accept(count.value + 1)
+    }
+    
 }
 
